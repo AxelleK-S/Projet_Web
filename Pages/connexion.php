22ac61c95d994802c6e75221f73510c457d3b798
DarylNoupik <daryl.kuitche.cm@viacesi.fr>
2022-11-24 11:56:30 +0100

    Modification du login

--- Pages/connexion.php
+++ Pages/connexion.php
@@ -14,45 +14,60 @@ function verification(){
 include("fonction.php");
 }?>
 
-<div class ="container">
- <h1 align="center"> Bienvenue sur notre site portfolio</h1>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-     <form method="post" action="fonction.php">
-    <div class="mb-3">
-        <p> </p>
-<p> </p>
-        <p>Veuillez entrer vos identifiants</p>
-        <label for="Email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Habitant@gmail.com" name="email" required>
-        <div id="emailHelp" class="form-text">Nous nallons jamais utiliser votre adresse à des fins autres.</div>
-    </div>
-    <div class="mb-3">
-        <label for="Password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="Password" minlength="8" name="password" required>
-        <div id="PasswordHelp" class="form-text">Votre mot de passe doit avoir minimum 8 caractères.</div>
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="Check" required>
-        <label class="form-check-label" for="Check">jaccepte l utilisation des cookies</label>
-    </div>
-    <button type="submit" class="btn btn-info">Envoyer</button>
-
-</form>
+        <div class ="container" style="background-image: url(images/img3.jpg);">
+              <h1 align="center"> Bienvenue sur notre site portfolio</h1>
         </div>
-        <div class="col-6" id="test">
-            <p id="texte"><i>Connectez-vous pour consulter votre CV</i></p>
+
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                  <div class="card" style="border-radius: 1rem;">
+                    <div class="row g-0 ">
+                      <div class="col-md-6 col-lg-5 d-none d-md-block">
+                        <img src="images/img3.jpg"  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                      </div>
+                      <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                          <div class="card-body p-4 p-lg-5 text-black">
+
+                         <form method="post" action="fonction.php">
+
+                           <div class="d-flex align-items-center mb-3 pb-1">
+                              <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                              <span class="h1 fw-bold mb-0">Logo</span>
+                          </div>
+
+                          <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Veuillez entrer vos identifiants</h5>
+
+                                 <div class="form-outline mb-4">
+                                      <label for="Email" class="form-label">Email address</label>
+                                      <input type="email" class="form-control form-control-lg" id="Email" aria-describedby="emailHelp" placeholder="Habitant@gmail.com" name="email" required>
+                                      <div id="emailHelp" class="form-text">Nous nallons jamais utiliser votre adresse à des fins autres.</div>
+                                  </div>
+                                  <div class="form-outline mb-4">
+                                          <label for="Password" class="form-label">Password</label>
+                                          <input type="password" class="form-control form-control-lg" id="Password" minlength="8" name="password" required>
+                                          <div id="PasswordHelp" class="form-text">Votre mot de passe doit avoir minimum 8 caractères.</div>
+                                  </div>
+                                <div class="form-outline mb-4 form-check">
+                                  <input type="checkbox" class="form-check-input" id="Check" required>
+                                  <label class="form-check-label" for="Check">jaccepte l utilisation des cookies</label>
+                               </div>
+                               <div class="pt-1 mb-4">
+                                   <button type="submit" class="btn btn-info">Envoyer</button>
+                               </div>
+
+                            </form>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+      </div>
 
-</div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
